be91f375f22806c7cf1d1dd4ff80a9a5942333e3
pamarsh907 <peter_aaron_marsh@hotmail.com>
2023-07-10 14:58:24 -0800

    added error handling

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 export default  async function WebSearchPage({searchParams}) {
@@ -5,8 +6,23 @@ export default  async function WebSearchPage({searchParams}) {
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   )
 
+  if(!response.ok){
+    throw new Error("Something went wrong");
+  }
   const data = await response.json()
   const results = data.items
+
+  if(!results){
+    return <div className='flex flex-col justify-center items-center pt-10'>
+        <h1 className='text-3xl mb-4'>No results found</h1>
+        <p className='text-lg'>
+            Try searching for something else or return to the {" "}
+            <Link href="/" className='text-blue-500'>
+                homepage
+            </Link>
+        </p>
+    </div>
+  }
     return (
     <div>
         {results && results.map(result=> <h1>{result.title}</h1>)}
